1095e8529774fc9b277a4702e441e219c441100e
Yamine Haddar <yhaddar@student.42.fr>
2018-01-10 18:38:48 +0100

    ex02 done

--- ex02/oddeven.php
+++ ex02/oddeven.php
@@ -1,14 +1,21 @@
 #!/usr/bin/php
 <?php
-print("Entrez un nombre: ");
-$input = fgets(STDIN);
-if (is_numeric($input) == 1){
-if ($input % 2 == 0)
-	print("Le chiffre $input est Pair\n");
-else
-	print("Le chiffre $input est Impair\n");
-}
-else
-	print("$input n'est pas un chiffre\n");
-?>
 
+$fd = fopen("php://stdin", "r");
+while ($fd && !feof($fd)) {
+	print("Entrez un nombre: ");
+	$input = fgets($fd);
+if ($input) {
+	$input = str_replace("\n", "", $input);
+	if (is_numeric($input)) {
+		if ($input % 2 == 0){
+			print("Le chiffre $input est Pair\n");
+		} else {
+			print("Le chiffre $input est Impair\n");
+	}
+} else {
+	print("$input n'est pas un chiffre\n");
+	}}}
+fclose($fd);
+print("\n");
+?>
