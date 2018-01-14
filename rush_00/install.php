d66df4f80735fe6d3b2e346b735364094363d7d9
Yamine HADDAR <yhaddar@e3r7p16.42.fr>
2018-01-14 20:33:48 +0100

    rushed rush

---
+++ rush_00/install.php
@@ -0,0 +1,15 @@
+<?php
+	$link = mysqli_connect("localhost", "root", "root", "") or die("Error ".mysql_error($link));
+	// include 'connect.php';
+	if (isset($_POST['submit']))
+	{
+		$sql = file_get_contents("db.sql");
+		$sql_array = explode(";", $sql);
+		foreach ($sql_array as $p)
+			mysqli_query($link, $p);
+	}
+?>
+
+<form action="install.php" method="post">
+	<input type="submit" name="submit" value="INSTALL"/>
+</form>
